af8d8589f1173179a24a5f935e234046c1b39d6b
Zeran Lu <zeranlu0326@gmail.com>
2026-05-09 19:16:43 -0400

    added imports into script tag in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script setup>
+    import HeroSection from '@/components/HeroSection.vue'
+    import AboutSection from '@/components/AboutSection.vue'
+    import SkillsSection from '@/components/SkillsSection.vue'
+    import FeaturedProjects from '@/components/FeaturedProjects.vue'
+    import ContactMini from '@/components/ContactMini.vue'
 </script>
 
 <style scoped lang="scss">
