1c78ea6e76399e03f0bddf245f2718bb0ad19264
Zeran Lu <zeranlu0326@gmail.com>
2026-05-10 19:58:07 -0400

    updated components folder structure
    files are better organized

---
+++ src/components/home-page/SkillsSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="hero-section">
+        <h2 class="hidden">Skills Section</h2>
+
+        <div class="skills-con">
+            <div class="skills-flex-con">
+
+            </div>
+
+            <div class="skills-flex-con">
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+</style>
