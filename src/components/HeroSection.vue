7d0d06ff4f6412745d5b26a31d3ebd48c92ea163
Zeran Lu <zeranlu0326@gmail.com>
2026-05-10 18:47:24 -0400

    added hero-section template

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,6 +1,15 @@
 <template>
-    <section class="hero-section">
-        <h2>Hero Section</h2>
+    <section class="hero-section content-con">
+        <h2 class="hidden">Hero Section</h2>
+        
+        <div class="hero-section-flex-item">
+            <h3>Hello! My name is Zeran Lu</h3>
+            <p>I am a front-end developer</p>
+        </div>
+
+        <div class="hero-section-flex-item">
+            <img src="@/assets/images/me.svg" alt="illustration of me" />
+        </div>
     </section>
 </template>
 
