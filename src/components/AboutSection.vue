000d8dab0ae53b1c8fb780ddf035106cc7c9f4c5
Zeran Lu <zeranlu0326@gmail.com>
2026-05-10 19:39:09 -0400

    added text content to AboutSection.vue

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,6 +1,24 @@
 <template>
-    <section class="hero-section">
+    <section class="about-section content-con">
         <h2>About Section</h2>
+
+        <div class="text-con">
+            <p>
+                I am Zeran, nice to meet you!
+                <br>
+                <br>
+                I am a <a class="highlight" href="https://www.w3schools.com/whatis/whatis_frontenddev.asp">front-end developer</a> that is passionate about creating creatively interactive websites that push the boundaries of what's possible. I am a graduate at Fanshawe College under the <a class="highlight" href="https://www.fanshawec.ca/programs/idp3-interactive-media-design/next">Interactive Design and Media program (IDP)</a>.
+                <br>
+                <br>
+                I have many different personal and school projects on my portfolio that reflect my various hobbies to show off my design and coding capabilities.
+                <br>
+                <br>
+                I have an accumulation of many skills and creative tools to tell stories through the language of code. Over my 2 years of coding experience I have honed my abilities with:
+                <br>
+                <br>
+            </p>
+        </div>
+
     </section>
 </template>
 
