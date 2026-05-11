c44b42e5a3fd948357a8fb6885f56bf5360d0efc
Zeran Lu <zeranlu0326@gmail.com>
2026-05-10 20:10:47 -0400

    created Icon.vue component

---
+++ src/components/home-page/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="skills-icon">
+        <img :src="icon" :alt="name" />
+        <p>{{ name }}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        name: String,
+        icon: String
+    })
+</script>
+
+<style scoped lang="scss">
+</style>
