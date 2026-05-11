a70897c582b893be5cb647162ce4375bf82d8ed5
Zeran Lu <zeranlu0326@gmail.com>
2026-05-11 03:59:35 -0400

    imported hero section for consistent spacing

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,9 +2,16 @@
     <main>
         <h1 class="hidden">Contact Page</h1>
 
+        <HeroSection />
+        
+        <section class="contact-section content-con">
+            <h2>Contact Me</h2>
+        </section>
+    </main>
 </template>
 
 <script setup>
+    import HeroSection from '@/components/home-page/HeroSection.vue'
 </script>
 
 <style scoped lang="scss">
