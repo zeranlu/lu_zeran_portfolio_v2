2b6cae81a4633dd44fdd31ff07e7d170be3de4d8
Zeran Lu <zeranlu0326@gmail.com>
2026-05-13 01:58:57 -0400

    copied and extended defineProps from caseStudyCard

---
+++ src/components/case-studies-page/FeaturedCaseStudyCard.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+    defineProps({
+        title: String,
+        desc: String,
+        prob: String,
+        fgImg: String,
+        bgImg: String,
+        id: Number
+    })
+    
+</script>
