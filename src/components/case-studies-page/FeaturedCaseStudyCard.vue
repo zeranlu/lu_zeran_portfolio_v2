29b3699c83b596cbd4764d5b62f7579ec49b71ec
Zeran Lu <zeranlu0326@gmail.com>
2026-05-13 01:59:06 -0400

    added tempalte in reference to new props

--- src/components/case-studies-page/FeaturedCaseStudyCard.vue
+++ src/components/case-studies-page/FeaturedCaseStudyCard.vue
@@ -1,3 +1,21 @@
+<template>
+
+    <div class="featured-case-study-card">
+        <RouterLink :to="`/case-study/${id}`">
+            <h3>{{ title }}</h3>
+            <p>{{ desc }}</p>
+            <p>{{ prob }}</p>
+
+            <div class="featured-case-study-card-img-con">
+                <img class="case-study-img-fg" :src="fgImg" :alt="title">
+                <img class="case-study-img-bg" :src="bgImg" alt="">
+            </div>
+
+        </RouterLink>
+    </div>
+
+</template>
+
 <script setup>
 
     defineProps({
