44b0af4d220fdd3900436a59818c5ad6dda07de5
Zeran Lu <zeranlu0326@gmail.com>
2026-05-13 01:05:22 -0400

    added variables to hold fetch'ed backend information

--- src/views/CaseStudies.vue
+++ src/views/CaseStudies.vue
@@ -6,6 +6,12 @@
 
 <script setup>
     import { ref } from 'vue'
+
+    const caseStudies = ref([])
+
+    const isLoading = ref(false)
+
+    const error = ref('')
 </script>
 
 <style scoped lang="scss">
