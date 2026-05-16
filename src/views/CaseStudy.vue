90047a0e8d440cb0c6b6632508b0927a29efb6b8
Zeran Lu <zeranlu0326@gmail.com>
2026-05-16 01:32:18 -0400

    imported HeroSection

--- src/views/CaseStudy.vue
+++ src/views/CaseStudy.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <h1 class="hidden">Specific Case Study</h1>
+
+        <HeroSection />
     </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue'
-    
+    import { ref, onMounted } from 'vue'
+    import HeroSection from '@/components/home-page/HeroSection.vue'
 </script>
 
 <style scoped lang="scss">
