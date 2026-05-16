6f49b2b12ae850190b29948199e59f5fc9671723
Zeran Lu <zeranlu0326@gmail.com>
2026-05-16 01:32:48 -0400

    added const variables necessary to complete the import of caseStudyInfo

--- src/views/CaseStudy.vue
+++ src/views/CaseStudy.vue
@@ -9,6 +9,14 @@
 <script setup>
     import { ref, onMounted } from 'vue'
     import HeroSection from '@/components/home-page/HeroSection.vue'
+
+    const caseStudyInfo = ref(null)
+
+    const caseStudyId = route.params.id
+
+    const isLoading = ref (false)
+
+    const error = ref('')
 </script>
 
 <style scoped lang="scss">
