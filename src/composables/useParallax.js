b7be91b4adfe3bf179318579bb897cae956f0b4a
Zeran Lu <zeranlu0326@gmail.com>
2026-05-22 01:23:57 -0400

    migrated pin: true logic

--- src/composables/useParallax.js
+++ src/composables/useParallax.js
@@ -2,3 +2,22 @@ import { onMounted, onUnmounted } from 'vue'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+export function useParallax() {
+    
+    // Waits for DOM to load
+    const runParallax = () => {
+        gsap.registerPlugin(ScrollTrigger)
+
+        document.querySelectorAll('.parallax-section').forEach((section) => {
+            gsap.to(section, {
+                scrollTrigger: {
+                    trigger: section,
+                    start: 'top top',
+                    end: '+=500',
+                    pin: true,
+                    scrub: 2
+                }
+            })
+        })
+    }
+}
