d4f921856fcd7de7ad1de1acac0b9d12e3c09493
Zeran Lu <zeranlu0326@gmail.com>
2026-05-25 05:01:16 -0400

    Create useBreakPointAnimation.js

---
+++ src/composables/useBreakPointAnimation.js
@@ -0,0 +1,28 @@
+import { onUnmounted } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export function useBreakPointAnimation() {
+
+    const runBreakPointAnimation = () => {
+        gsap.registerPlugin(ScrollTrigger)
+
+        document.querySelectorAll('.breakpoint-section').forEach((section) => {
+            gsap.fromTo(section.querySelector('h2'),
+                {opacity: 0},
+            {
+                opacity: 1,
+                trigger: section,
+                start: 'top center',
+                end: 'bottom center',
+                scrub: 2
+            })
+        })
+    }
+
+    onUnmounted(() => {
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+    })
+
+    return { runBreakPointAnimation }
+}
