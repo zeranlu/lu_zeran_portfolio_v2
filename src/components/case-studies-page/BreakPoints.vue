217df8295aad653ff67eb3874956091bc3b584b8
Zeran Lu <zeranlu0326@gmail.com>
2026-05-25 04:32:21 -0400

    added breakPointsProps

--- src/components/case-studies-page/BreakPoints.vue
+++ src/components/case-studies-page/BreakPoints.vue
@@ -1,7 +1,13 @@
 <template>
+    <div class="parallax-con">
+        <h2>{{ breakPoint }}</h2>
+    </div>
 </template>
 
 <script setup>
+    const breakPointsProps = defineProps({
+        breakPoint: String
+    })
 </script>
 
 <style scoped lang="scss">
