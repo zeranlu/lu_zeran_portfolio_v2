7a418b1e55b8a3cafe964b5cf392b216874c4719
Zeran Lu <zeranlu0326@gmail.com>
2026-09-24 16:59:37 -0400

    update template skeleton
    removed incorrect id's and eliminated extra root elements

--- src/components/case-study-components/ProjectBeginnings.vue
+++ src/components/case-study-components/ProjectBeginnings.vue
@@ -1,39 +1,45 @@
 <template>
-    <div id="project-beginning">
+    <div class="project-beginning">
         <h2>Project Beginnings</h2>
 
-        <div id="project-beginning-imgcon">
+        <div class="project-beginning-imgcon">
             <p>images goes here</p>
+        </div>
+
+        <div class="project-beginning-list-con">
+
+            <div class="project-specs">
+                <ul class="project-specs-list">
+                    <li class="project-specs-li">
+                        Project Spec 1
+                    </li>
+                    <li class="project-specs-li">
+                        Project Spec 2
+                    </li>
+                    <li class="project-specs-li">
+                        Project Spec 3
+                    </li>
+                </ul>
+            </div>
+
+            <div class="personal-goals">
+                <ul class="personal-goals-list">
+                    <li class="personal-goals-li">
+                        Personal Goal 1
+                    </li>
+                    <li class="personal-goals-li">
+                        Personal Goal 2
+                    </li>
+                    <li class="personal-goals-li">
+                        Personal Goal 3
+                    </li>
+                </ul>
+            </div>
+
         </div>
     </div>
 
-    <div id="project-specs">
-        <ul id="project-specs-list">
-            <li id="project-specs-li">
-                Project Spec 1
-            </li>
-            <li id="project-specs-li">
-                Project Spec 2
-            </li>
-            <li id="project-specs-li">
-                Project Spec 3
-            </li>
-        </ul>
-    </div>
 
-    <div id="personal-goals">
-        <ul id="personal-goals-list">
-            <li id="personal-goals-li">
-                Personal Goal 1
-            </li>
-            <li id="personal-goals-li">
-                Personal Goal 2
-            </li>
-            <li id="personal-goals-li">
-                Personal Goal 3
-            </li>
-        </ul>
-    </div>
 </template>
 
 <script>
