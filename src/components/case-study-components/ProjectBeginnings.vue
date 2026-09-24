597f81cc0a8c1ff520c669563cf7554562172f3f
Zeran Lu <zeranlu0326@gmail.com>
2026-09-24 16:49:08 -0400

    Project Beginning basic skeleton added

--- src/components/case-study-components/ProjectBeginnings.vue
+++ src/components/case-study-components/ProjectBeginnings.vue
@@ -1,4 +1,39 @@
 <template>
+    <div id="project-beginning">
+        <h2>Project Beginnings</h2>
+
+        <div id="project-beginning-imgcon">
+            <p>images goes here</p>
+        </div>
+    </div>
+
+    <div id="project-specs">
+        <ul id="project-specs-list">
+            <li id="project-specs-li">
+                Project Spec 1
+            </li>
+            <li id="project-specs-li">
+                Project Spec 2
+            </li>
+            <li id="project-specs-li">
+                Project Spec 3
+            </li>
+        </ul>
+    </div>
+
+    <div id="personal-goals">
+        <ul id="personal-goals-list">
+            <li id="personal-goals-li">
+                Personal Goal 1
+            </li>
+            <li id="personal-goals-li">
+                Personal Goal 2
+            </li>
+            <li id="personal-goals-li">
+                Personal Goal 3
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
