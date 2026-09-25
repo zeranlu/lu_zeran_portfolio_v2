26c063892b26d910eece32660418d46e2bbd02f6
Zeran Lu <zeranlu0326@gmail.com>
2026-09-25 16:17:50 -0400

    ported basic skeleton, will work on this later

--- src/components/case-study-components/StrategicLimitations.vue
+++ src/components/case-study-components/StrategicLimitations.vue
@@ -1,4 +1,14 @@
 <template>
+    <div class="strategic-limitations content-con">
+
+        <h2>Strategic Limitations</h2>
+
+        <p> {{ caseStudyInfo.proj_problem }} </p>
+
+        <div class="case-study-img-con">
+            <img class="case-study-img" :src="caseStudyInfo.proj_image_link_2" :alt="`picture of ${caseStudyInfo.project_title} problem`">
+        </div>
+    </div>
 </template>
 
 <script setup>
