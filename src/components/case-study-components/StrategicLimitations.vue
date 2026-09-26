1505d55880ca784f3b8a0af7d2f2f6915133c957
Zeran Lu <zeranlu0326@gmail.com>
2026-09-26 11:10:25 -0400

    updated skeleton content, solution and problem content within ul of component parent element

--- src/components/case-study-components/StrategicLimitations.vue
+++ src/components/case-study-components/StrategicLimitations.vue
@@ -5,9 +5,54 @@
 
         <p> {{ caseStudyInfo.proj_problem }} </p>
 
-        <div class="case-study-img-con">
-            <img class="case-study-img" :src="caseStudyInfo.proj_image_link_2" :alt="`picture of ${caseStudyInfo.project_title} problem`">
-        </div>
+        <ul class="limitation-list">
+
+            <h3>Isolated Problems</h3>
+
+            <li>
+                <h4> {{ caseStudyInfo.proj_problem }} </h4>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_2" :alt="`picture of ${caseStudyInfo.project_title} problem`">
+                </div>
+
+                <p>{{ caseStudyInfo.proj_solution }}</p>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_3" :alt="`picture of ${caseStudyInfo.project_title}'s solution`">
+                </div>
+            </li>
+
+            <li>
+                <h4> {{ caseStudyInfo.proj_problem }} </h4>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_2" :alt="`picture of ${caseStudyInfo.project_title} problem`">
+                </div>
+
+                <p>{{ caseStudyInfo.proj_solution }}</p>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_3" :alt="`picture of ${caseStudyInfo.project_title}'s solution`">
+                </div>
+            </li>
+
+            <li>
+                <h4> {{ caseStudyInfo.proj_problem }} </h4>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_2" :alt="`picture of ${caseStudyInfo.project_title} problem`">
+                </div>
+
+                <p>{{ caseStudyInfo.proj_solution }}</p>
+
+                <div class="case-study-img-con">
+                    <img class="case-study-img" :src="caseStudyInfo.proj_image_link_3" :alt="`picture of ${caseStudyInfo.project_title}'s solution`">
+                </div>
+            </li>
+
+        </ul>
+
     </div>
 </template>
 
